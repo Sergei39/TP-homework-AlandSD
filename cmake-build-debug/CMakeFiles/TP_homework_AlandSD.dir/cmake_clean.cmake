f4d143c88dc59367e20bde129b97e35bef46d18d
Sergei Shishkin <3shishkin39@mail.ru>
2020-10-13 20:33:43 +0300

    add Project Files

---
+++ cmake-build-debug/CMakeFiles/TP_homework_AlandSD.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_homework_AlandSD.dir/main.cpp.o"
+  "TP_homework_AlandSD"
+  "TP_homework_AlandSD.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_homework_AlandSD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TP_homework_AlandSD.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TP_homework_AlandSD.
+# This may be replaced when dependencies are built.
